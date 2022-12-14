c9dd85676a828a5f5599571229938c92f6473267
Ayansaxena24 <ayan.saxena24@gmail.com>
2022-12-14 15:16:37 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path='/' index element={<Home />}></Route>
+          <Route path="/IMDB-Clone" index element={<Home />}></Route>
           <Route path="movie/:id" element={<Movie />}></Route>
           <Route path="movies/:type" element={<MovieList />}></Route>
-          <Route path="/*" element={<h1>Error Page</h1>}></Route>
+          {/* <Route path="/*" element={<h1>Error Page</h1>}></Route> */}
         </Routes>
       </Router>
     </div>
