e86051b2088b3737a64c9d38f1a30cbc499aa549
Ayan Saxena <84512148+Ayansaxena24@users.noreply.github.com>
2022-12-15 00:45:41 +0530

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className="header">
             <div className="headerLeft">
-                <Link to="/"><img id="header_icon" alt="imdb" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" /></Link>
+                <Link to="/IMDB-Clone"><img id="header_icon" alt="imdb" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" /></Link>
                 <Link to="/movies/popular"><span>Popular</span></Link>
                 <Link to="/movies/top_rated"><span>Top Rated</span></Link>
                 <Link to="/movies/upcoming"><span>Upcoming</span></Link>
